027a8b483950dad3b5660ab2ec623595d064fa93
Armin Ale <arminale1@gmail.com>
2023-12-11 19:49:11 -0500

    Day 9 Q1

---
+++ 2023/9/1/answer.py
@@ -0,0 +1,18 @@
+import numpy as np
+from numpy.polynomial import Polynomial as P
+
+
+def main():
+    extrapolated_sum = 0
+    with open("input.txt") as f:
+        for line in f:
+            sequence = np.asarray([int(x) for x in line.strip().split()])
+            ind = np.arange(sequence.shape[0])
+            p = P.fit(ind, sequence, sequence.shape[0] - 1).convert()
+            extrapolated_sum += int(np.rint(p(sequence.shape[0])))
+
+    print(extrapolated_sum)
+
+
+if __name__ == "__main__":
+    main()
